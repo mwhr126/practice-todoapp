212f735912048dacc472a251e3393230a6a3f6b1
miwa <dskipineapple@gmail.com>
2024-10-07 16:20:22 +0900

    fakeの実行、show（ボード詳細ページ）の作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,11 @@
 class BoardsController < ApplicationController
 
   def index
-    @board = Board.first
+    @boards = Board.all
   end
 
-  def board
+  def show
+    @board = Board.find(params[:id])
   end
 
 end
