1d4c4bbd91a185f8f1929f956775427a24b456df
miwa <dskipineapple@gmail.com>
2024-10-08 12:16:58 +0900

    boardを新規追加できるように

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,4 +8,22 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
+  end
 end
